1a9e676bff9a625818d78943f58e6d122884865d
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-06-16 13:44:01 +0600

    added user in mongodb

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const port = 5000
-require("dotenv").config();
-
-// the mighty comment
-// the mighty comment
-// the mighty comment
-// the mighty comment
-// the mighty comment
-// the mighty comment
-// the mighty comment
-
 const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('server is doing sit up');
 })
 
 // mongo code goes here
-// console.log(process.env.USER);
-// console.log(process.env.PASS);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ta7i6kc.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://prodipkrishna01:C0dhtgOsm5WfWYBc@cluster0.ta7i6kc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,10 +32,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const classCollection = client.db("getFit").collection('class')
     const trainerCollection = client.db("getFit").collection('trainer')
+    const cartsCollection = client.db("getFit").collection('carts')
+    const userCollection = client.db("getFit").collection('users')
+
+    // users
+    app.post('/users', async(req, res) => {
+      // const user = req.body;
+      // const result = await userCollection.insertOne(user)
+      // res.send(result)
+      try {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+      } catch (error) {
+        console.error("Failed to insert user data:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    })
+
+    // add carts items
+    app.post('/carts', async(req, res) => {
+      const item = req.body
+      console.log(item)
+      const result = await cartsCollection.insertOne(item)
+      res.send(result)
+    })
+    
 
     // get all classes
     app.get('/class', async (req, res) => {
